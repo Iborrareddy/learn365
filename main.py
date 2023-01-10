d97550f3c83d6f00cd68cf11464b8d8b6243f9cc
Borra Madhan Mohan Reddy <49640840+Iborrareddy@users.noreply.github.com>
2023-01-10 07:32:29 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,34 @@
+Print 
+print('Hello world !')
+print("Hello"+"World !")
+print('Hello world ! \n Hello World \n Hello World')
+----------------------------------------
+#  Print() string formatting 
+name = "iborrareddy"
+age = 23
+print('My name is {} and my age {}'.format(name,age))
+
+#Print f-String
+name = 'iborrareddy'
+age = 23
+print(f'My name is {name} and my age {age}')
+-------------------------------------
+#Input
+print("Hello " + input("Enter the name:")) 
+
+#length
+print(len("iborrareddy"))
+print(len(input("Enter the name to get the length:")))
+
+
+a=10
+b=20
+
+#a,b = b,a
+
+c = a
+a = b
+b = c
+print(f'a=10:{a} and b=20:{b}')
+
+
